0dfb55c78e5f3e9217f13b5500bc5eb68d83f97b
Pierre Rudloff <contact@rudloff.pro>
2016-11-21 23:11:48 +0100

    Lint

--- ArchiveLinks.php
+++ ArchiveLinks.php
@@ -4,13 +4,12 @@ namespace ArchiveLinks;
 
 class ArchiveLinks
 {
-
-    static function onLinkerMakeExternalLink(&$url, &$text, &$link, array &$attribs, $linktype)
+    public static function onLinkerMakeExternalLink(&$url, &$text, &$link, array &$attribs, $linktype)
     {
         $attribs['href'] = $url;
         $archiveAttribs = [
-            'rel'=>$attribs['rel'],
-            'href'=>'https://web.archive.org/web/*/'.$url
+            'rel'  => $attribs['rel'],
+            'href' => 'https://web.archive.org/web/*/'.$url,
         ];
         if (isset($attribs['target'])) {
             $archiveAttribs['target'] = $attribs['target'];
@@ -20,5 +19,4 @@ class ArchiveLinks
         //We need to return false if we want to modify the HTML of external links
         return false;
     }
-
 }
