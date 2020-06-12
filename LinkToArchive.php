691ab8440518819d8ae30b64ad65ae2beddc9afe
Pierre Rudloff <prudloff@insite.coop>
2020-06-12 11:30:52 +0200

    Lint

--- LinkToArchive.php
+++ LinkToArchive.php
@@ -2,8 +2,18 @@
 
 namespace LinkToArchive;
 
+use Html;
+
 class LinkToArchive
 {
+    /**
+     * @param $url
+     * @param $text
+     * @param $link
+     * @param array $attribs
+     * @param $linktype
+     * @return bool
+     */
     public static function onLinkerMakeExternalLink($url, $text, &$link, array &$attribs, $linktype)
     {
         if ($linktype
@@ -11,16 +21,18 @@ class LinkToArchive
         ) {
             $attribs['href'] = $url;
             $archiveAttribs = [
-                'rel'  => $attribs['rel'],
-                'href' => 'https://web.archive.org/web/'.$url,
+                'rel' => $attribs['rel'],
+                'href' => 'https://web.archive.org/web/' . $url,
             ];
             if (isset($attribs['target'])) {
                 $archiveAttribs['target'] = $attribs['target'];
             }
-            $link = \Html::rawElement('a', $attribs, $text).' <sup>'.\Html::rawElement('a', $archiveAttribs, '['.wfMessage('archive')->parse().']').'</sup>';
+            $link = Html::rawElement('a', $attribs, $text) . ' <sup>' . Html::rawElement('a', $archiveAttribs, '[' . wfMessage('archive')->parse() . ']') . '</sup>';
 
             //We need to return false if we want to modify the HTML of external links
             return false;
         }
+
+        return null;
     }
 }
